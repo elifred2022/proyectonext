c8f419a6b4c7d3e7283f2489cce5b5cf1b4d7f19
elifred2022 <elifed21@hotmail.com>
2025-06-01 16:22:32 -0300

    separado el users del page principal

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Users from "./components/Users";
+
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users?page=2");
+  const data = await res.json();
+  console.log(data);
+  return data.data; // Esto sí es un array
+}
+
+async function HomePage() {
+  const users = await fetchUsers();
+
+  return (
+    <div>
+      <Users users={users} />
+    </div>
+  );
+}
+
+export default HomePage;
